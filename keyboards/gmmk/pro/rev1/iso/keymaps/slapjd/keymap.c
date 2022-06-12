77d8de14903bd92cc673f7300617323767baf5ad
slapjd <slapjd@outlook.com>
2022-06-12 10:01:10 +0100

    More RGB and migrate encoder to encoder map

--- keyboards/gmmk/pro/rev1/iso/keymaps/slapjd/keymap.c
+++ keyboards/gmmk/pro/rev1/iso/keymaps/slapjd/keymap.c
@@ -18,7 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum layers {
   _BASE,
-  _FN
+  _FN,
+  _RH
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -41,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          _______,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   _______,
-        MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           _______,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   RGB_MOD,
+        MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           RGB_RMOD,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   _______,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -50,34 +51,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN] = LAYOUT(
         RGB_TOG, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  EEP_RST,          KC_MPLY,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,           _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+        _______, _______, _______, _______, MO(_RH), _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, RESET,   NK_TOGG, _______, _______, _______, _______,          _______, KC_PGUP, _______,
         _______, _______, KC_CAPS,                            _______,                            _______, _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
 
+    [_RH] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
+    ),
 
 };
 
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [_BASE] =     { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_FN]   =     { ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+    [_RH] =  { ENCODER_CCW_CW(RGB_HUD, RGB_HUI) }
+};
+#endif
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    switch (get_highest_layer(layer_state|default_layer_state)){
-      case _FN:
-        if (clockwise) {
-          tap_code(KC_MNXT);
-        } else {
-          tap_code(KC_MPRV);
-        }
-        break;
+
+// bool encoder_update_user(uint8_t index, bool clockwise) {
+//     switch (get_highest_layer(layer_state|default_layer_state)){
+//       case _RGB_HUE:
+//         if (clockwise) {
+//           rgb_matrix_increase_hue();
+//         } else {
+//           rgb_matrix_decrease_hue();
+//         }
+//         break;
+
+//       case _FN:
+//         if (clockwise) {
+//           tap_code(KC_MNXT);
+//         } else {
+//           tap_code(KC_MPRV);
+//         }
+//         break;
       
-      default:
-        if (clockwise) {
-          tap_code(KC_VOLU);
-        } else {
-          tap_code(KC_VOLD);
-        }
-        break;
-    }
+//       default:
+//         if (clockwise) {
+//           tap_code(KC_VOLU);
+//         } else {
+//           tap_code(KC_VOLD);
+//         }
+//         break;
+//     }
     
-    return true;
-}
+//     return true;
+// }
--- keyboards/gmmk/pro/rev1/iso/keymaps/slapjd/rules.mk
+++ keyboards/gmmk/pro/rev1/iso/keymaps/slapjd/rules.mk
@@ -1,3 +1,6 @@
 #Attempted reduction in latency
 DEBOUNCE_TYPE = sym_eager_pk
 DEBOUNCE = 2
+
+#Encoder keymap functionality
+ENCODER_MAP_ENABLE = yes
