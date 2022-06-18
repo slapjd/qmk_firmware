f2db43b10552ce5013b6ac72299fadbaca0122b1
slapjd <slapjd@outlook.com>
2022-06-18 08:19:01 +0100

    Debounce business

--- keyboards/gmmk/pro/rev1/iso/keymaps/slapjd/config.h
+++ keyboards/gmmk/pro/rev1/iso/keymaps/slapjd/config.h
@@ -1,10 +1,18 @@
 // Higher Poll Rate
 #define USB_POLLING_INTERVAL_MS 1
 
+//Debounce time
+//We use eager per-key so this is basically the minimum
+//time between multiple presses of *the same* key
+//this could be like 100 and we probably wouldn't give a shit
+#undef DEBOUNCE
+#define DEBOUNCE 40
+
 //Neat RGB effex
 #ifdef RGB_MATRIX_ENABLE
     #define RGB_MATRIX_KEYPRESSES
     #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+    #define RGB_MATRIX_TYPING_HEATMAP_SLIM //Regular heatmap borked
     #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SPLASH
     #define RGB_DISABLE_WHEN_USB_SUSPENDED
 #endif
--- keyboards/gmmk/pro/rev1/iso/keymaps/slapjd/rules.mk
+++ keyboards/gmmk/pro/rev1/iso/keymaps/slapjd/rules.mk
@@ -1,6 +1,5 @@
 #Attempted reduction in latency
 DEBOUNCE_TYPE = sym_eager_pk
-DEBOUNCE = 2
 
 #Encoder keymap functionality
 ENCODER_MAP_ENABLE = yes
